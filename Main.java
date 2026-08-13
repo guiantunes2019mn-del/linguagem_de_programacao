c5c3082d6ddeed86156eb3f09d816eb54d07f231
guiantunes2019mn-del <guiantunes2019mn@gmail.com>
2026-08-12 22:30:26 -0300

    primeiro códico

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main{
+    public static void main (String[] args) {
+        String aluno_1 = "Joãozinho";
+        String aluno_2 = "Mariazinha";
+        System.out.println("O aluno 1: " + aluno_1);
+        System.out.println("O aluno 2: " + aluno_2);
+        System.out.println("eh casado com " + aluno_2);
+
+
+    
+
+    }
+}
+
